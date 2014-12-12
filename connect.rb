965f1c28e91f1cbdb6d84d8c6a56d7a850869856
amfipter <amfipter@gmail.com>
2014-12-12 21:04:40 +0600

    connection

--- connect.rb
+++ connect.rb
@@ -1,13 +1,44 @@
+require 'socket'
 module Connection
   def self.broadcast()
-    nil
+	  	t = Thread.new do
+	    addr = ['<broadcast>', 33333]# broadcast address
+		#addr = ('255.255.255.255', 33333) # broadcast address explicitly [might not work ?]
+		#addr = ['127.0.0.255', 33333] # ??
+		udp = UDPSocket.new
+		udp.setsockopt(Socket::SOL_SOCKET, Socket::SO_BROADCAST, true)
+		data = $out_port.to_s
+		while($broadcast)
+			udp.send(data, 0, addr[0], addr[1])
+			sleep 1
+		end
+		udp.close
+	end
+	# t.join
   end
 
   def self.connection()
-    nil
+	addr = ['0.0.0.0', 33333]  # host, port
+	BasicSocket.do_not_reverse_lookup = true
+	# Create socket and bind to address
+	udp = UDPSocket.new
+	udp.bind(addr[0], addr[1])
+	data, addr = udp.recvfrom(1024) # if this number is too low it will drop the larger packets and never give them to you
+	puts "From addr: '%s', msg: '%s'" % [addr[2], data]
+	udp.close
+	$out_port = data.to_i + 1
+
+	$left_client = TCPSocket.open(addr[2], data.to_i)
+	puts 'connected: ' + addr[2].to_s + ' ' + data
   end
 
   def self.connect()
-    nil
+    t = Thread.new do 
+    	server = TCPServer.open($out_port)
+    	puts "TCP started at " + $out_port.to_s
+    	$right_client = server.accept
+    	$broadcast = false
+    end
+    # t.join
   end
 end
--- main.rb
+++ main.rb
@@ -1,1 +1,22 @@
 require './connect.rb'
+
+$broadcast = true
+$out_port = 30000
+$left_client = nil
+$right_client = nil
+
+if(ARGV[0].eql? '0')
+	puts '0'
+	Connection::broadcast()
+	Connection::connect()
+elsif(ARGV[0].eql? '1')
+	puts '1'
+	Connection::connection()
+	Connection::broadcast()
+	Connection::connect()
+elsif(ARGV[0].eql? '2')
+	Connection::connection()
+end
+
+Thread.list.each { |thr| thr.join if thr != Thread.main}
+
